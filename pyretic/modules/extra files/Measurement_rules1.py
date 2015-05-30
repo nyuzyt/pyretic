a41c591135e8f2e41e80dea96b1c97652b74f097
vishleshpatel <vishlesh.patel@nyu.edu>
2015-05-30 00:10:23 -0400

    change in path

--- pyretic/modules/extra files/Measurement_rules1.py
+++
@@ -1,9 +0,0 @@
-
-from pyretic.lib.corelib import *
-from pyretic.lib.std import *
-from pyretic.lib.query import *
-
-def main():
-	forward1 = if_(match(srcip = '10.0.0.0/31'),fwd(1))
-	return forward1
-
